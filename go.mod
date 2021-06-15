9992612cd90b98259764971568f0bb31b92769f6
nat <mrnatande@gmail.com>
2021-06-14 23:21:47 -0400

    Test

--- go.mod
+++ go.mod
@@ -3,16 +3,17 @@ module github.com/labagroup/mobile
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/golang/geo v0.0.0-20210211234256-740aa86cb551 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/uuid v1.2.0
 	github.com/gopub/conv v0.6.1
 	github.com/gopub/errors v0.1.7
 	github.com/gopub/log v1.2.8
-	github.com/gopub/sql v1.4.18 // indirect
-	github.com/gopub/types v0.3.6
+	github.com/gopub/sql v1.4.18
+	github.com/gopub/types v0.3.19
 	github.com/gopub/wine/urlutil v0.1.5
 	github.com/mattn/go-sqlite3 v1.14.7 // indirect
-	github.com/nyaruka/phonenumbers v1.0.65
+	github.com/nyaruka/phonenumbers v1.0.70
 	github.com/shopspring/decimal v1.2.0 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
 )
--- sqltable_test.go
+++ sqltable_test.go
@@ -2,8 +2,11 @@ package mobile_test
 
 import (
 	"database/sql"
+	"github.com/gopub/errors"
+	"github.com/gopub/types"
 	"github.com/labagroup/mobile"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -14,9 +17,7 @@ func createTable(t *testing.T, name string, keyType mobile.TableKeyType) *mobile
 	}
 
 	tbl, err := mobile.NewTable(db, name, mobile.TableKeyInt64)
-	if err != nil {
-		t.Error(err)
-	}
+	require.NoError(t, err)
 	return tbl
 }
 
@@ -30,22 +31,52 @@ func (i *testItem) RecordKey() interface{} {
 	return i.ID
 }
 
+func newTestItem() *testItem {
+	return &testItem{
+		ID:    types.RandomID().Int(),
+		Name:  types.RandomID().Pretty(),
+		Score: float64(types.RandomID()) / float64(3),
+	}
+}
+
 func TestIntTable(t *testing.T) {
-	tbl := createTable(t, "foo", mobile.TableKeyInt64)
-	err := tbl.Insert(&testItem{ID: 123, Name: "haha"})
-	if err != nil {
-		t.Error(err)
-	}
+	tbl := createTable(t, "tbl"+types.RandomID().Pretty(), mobile.TableKeyInt64)
+	var items []*testItem
+	item := newTestItem()
+	items = append(items, item)
+	err := tbl.Insert(item)
+	require.NoError(t, err)
 	var v *testItem
-	err = tbl.Get(123, &v)
-	if err != nil {
-		t.Error(err)
-	}
-	t.Log(v.ID, v.Name)
+	err = tbl.Get(item.ID, &v)
+	require.NoError(t, err)
+	require.Equal(t, item, v)
+
+	item = newTestItem()
+	item.ID = items[0].ID + 1
+	err = tbl.Insert(item)
+	require.NoError(t, err)
+	items = append(items, item)
 
 	var l []*testItem
 	err = tbl.ListAll(&l)
-	if err != nil {
-		t.Error(err)
-	}
+	require.NoError(t, err)
+	require.NotEmpty(t, l)
+	require.Equal(t, items, l)
+
+	l = l[0:0]
+	err = tbl.ListGreaterThan(item.ID, &l)
+	require.NoError(t, err)
+	require.Empty(t, l)
+
+	l = l[0:0]
+	err = tbl.ListLessThan(item.ID, &l)
+	require.NoError(t, err)
+	require.Equal(t, 1, len(l))
+
+	err = tbl.Delete(item.ID)
+	require.NoError(t, err)
+
+	err = tbl.Get(item.ID, &v)
+	require.NotEmpty(t, err)
+	require.Equal(t, true, errors.Is(err, sql.ErrNoRows))
 }
